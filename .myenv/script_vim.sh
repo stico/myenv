29bd14313e567d4549e4ade9a93c2d4aefff0a79
stico <ouyzhu@gmail.com>
2012-09-24 20:14:35 +0800

    script_vim compitable with cygwin path

--- .myenv/script_vim.sh
+++ .myenv/script_vim.sh
@@ -4,4 +4,10 @@ vimBin=${MY_PRO}/A_Text_Vim_7.2_PA-Basic/App/vim/vim72/gVim
 
 [ ! -n "$1" ] && echo '-!> At least set the 1st parameter, which indicating the file to edit' && exit
 
-${vimBin} $* &
+if [[ $(echo $1 | grep -c "^/cygdrive/") == 1 ]] ; then
+	cygpath -w $1
+	winPath=`cygpath -w $1`
+	shift
+fi
+
+${vimBin} $winPath $* &
