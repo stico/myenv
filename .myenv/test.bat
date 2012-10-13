11ff8872d11e215d1cc44de60c90d937ebaf3b37
unknown <ouyangzhu@.(none)>
2012-10-13 21:54:34 +0800

    remove path already in common file

--- .myenv/test.bat
+++ .myenv/test.bat
@@ -4,9 +4,6 @@
 For /f "tokens=* delims=" %%V in ('script_getWinVersion.bat') Do (set WinVersion=%%V)
 echo "Current win version is: %WinVersion%"
 
-IF "%WinVersion:~0,2%"=="XP" SET CmdSetX=setx 
-IF "%WinVersion:~0,4%"=="WIN7" SET CmdSetX="C:\Program Files\Support Tools\SETX" 
-ECHO Setting setx path: %CmdSetX% 
 
 
 pause
