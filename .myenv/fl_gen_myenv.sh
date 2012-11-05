5df003859f5d4063b115ffe26a3f65641c2b264c
ouyangzhu <ouyangzhu@workvm.(none)>
2012-11-05 23:22:45 +0800

    add fl_myenv.lst

---
+++ .myenv/fl_gen_myenv.sh
@@ -0,0 +1,13 @@
+
+src=$HOME/.gitignore 
+target=$MY_ENV/fl_myenv.list
+
+echo "Generating $target from $src file"
+
+sed -e "/^\s*$/d;		\
+	/^\s*#/d;		\
+	/\/\*/d;		\
+	/.fl_files.txt/d;	\
+	/.grep_result.txt/d;	\
+	/gVim.exe.stackdump/d;	\
+	s/^!//;"	$src > $target
