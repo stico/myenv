079452f128b20c74563d41a52b1afae157843834
unknown <ouyangzhu@.(none)>
2012-11-10 15:03:51 +0800

    add ddelete init script

--- .myenv/init/win.bat
+++ .myenv/init/win.bat
@@ -50,10 +50,12 @@ REM C:\WINDOWS\system32\reg add HKEY_CLASSES_ROOT\*\shell\VersionBackup\command 
 REM C:\WINDOWS\system32\reg add HKEY_CLASSES_ROOT\Directory\shell\VersionBackup\command /f /t REG_EXPAND_SZ /ve /d "%%MY_ENV%%\script_backupWithVersion.bat \"%%1\""
 REM reg add HKEY_CLASSES_ROOT\*\shell\VersionBackup\command /f /t REG_EXPAND_SZ /ve /d "%%MY_ENV%%\script_backupWithVersion.bat \"%%1\""
 REM reg add HKEY_CLASSES_ROOT\Directory\shell\VersionBackup\command /f /t REG_EXPAND_SZ /ve /d "%%MY_ENV%%\script_backupWithVersion.bat \"%%1\""
+REM reg add HKEY_CLASSES_ROOT\*\shell\DatedDelete\command /f /t REG_EXPAND_SZ /ve /d "%%MY_ENV%%\script_datedDelete.bat \"%%1\""
+REM reg add HKEY_CLASSES_ROOT\Directory\shell\DatedDelete\command /f /t REG_EXPAND_SZ /ve /d "%%MY_ENV%%\script_datedDelete.bat \"%%1\""
 reg add HKEY_CLASSES_ROOT\*\shell\DBackup\command /f /t REG_EXPAND_SZ /ve /d "%%MY_ENV%%\utilities\dated_backup.bat \"%%1\""
 reg add HKEY_CLASSES_ROOT\Directory\shell\DBackup\command /f /t REG_EXPAND_SZ /ve /d "%%MY_ENV%%\utilities\dated_backup.bat \"%%1\""
-reg add HKEY_CLASSES_ROOT\*\shell\DatedDelete\command /f /t REG_EXPAND_SZ /ve /d "%%MY_ENV%%\script_datedDelete.bat \"%%1\""
-reg add HKEY_CLASSES_ROOT\Directory\shell\DatedDelete\command /f /t REG_EXPAND_SZ /ve /d "%%MY_ENV%%\script_datedDelete.bat \"%%1\""
+reg add HKEY_CLASSES_ROOT\*\shell\DDelete\command /f /t REG_EXPAND_SZ /ve /d "%%MY_ENV%%\utilities\dated_delete.bat \"%%1\""
+reg add HKEY_CLASSES_ROOT\Directory\shell\DDelete\command /f /t REG_EXPAND_SZ /ve /d "%%MY_ENV%%\utilities\dated_delete.bat \"%%1\""
 
 ECHO Updating file association
 %MY_ENV%\script_fileAssoc-repeatable.bat
