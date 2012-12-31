eb537cbb7126b7966ec50fb4d04bf31918f41235
ouyangzhu <ouyangzhu@workvm.(none)>
2012-12-31 20:46:50 +0800

    update vim init script

--- .myenv/init/vim_plugin.sh
+++ .myenv/init/vim_plugin.sh
@@ -5,11 +5,11 @@
 declare -A plugin_addr
 plugin_addr["vim-colors-solarized"]="git://github.com/altercation/vim-colors-solarized.git"
 plugin_addr["vim-surround"]="git://github.com/tpope/vim-surround.git"
+plugin_addr["vim-pathogen"]="git://github.com/tpope/vim-pathogen.git"
 plugin_addr["vim-repeat"]="git://github.com/tpope/vim-repeat.git"
 
+# update plugins
 for plugin in "${!plugin_addr[@]}" ; do
-
-
 	if [ -e $VIM_CONF/bundle/$plugin/.git ] ; then
 		echo "Updateing plugin: $plugin"
 		cd $VIM_CONF/bundle/$plugin
@@ -20,3 +20,8 @@ for plugin in "${!plugin_addr[@]}" ; do
 		git clone ${plugin_addr[$plugin]} 
 	fi
 done
+
+# update pathogen
+echo "Updating pathogen by copy"
+[ -e $VIM_CONF/autoload ] && mv $VIM_CONF/autoload /tmp/
+cp -R $VIM_CONF/bundle/vim-pathogen/autoload/ $VIM_CONF/
