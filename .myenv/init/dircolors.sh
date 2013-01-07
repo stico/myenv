ed555e0e25e6aae9e13d6ac30bc1f05d5c1f682f
stico <ouyzhu@gmail.com>
2013-01-07 12:51:54 +0800

    update dir color conf naming

--- .myenv/init/dircolors.sh
+++ .myenv/init/dircolors.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-base=$MY_CONF/dircolors_solarized
-[ ! -e $base ] && echo "ERROR: $base not exist, pls check!" && exit
+[ ! -e $MY_ENV_CONF ] && echo "ERROR: $base not exist, pls check!" && exit
 
-cd $base
+base=$MY_ENV_CONF/dircolors-solarized
 if [ -e $base/.git ] ; then
+	cd $base
 	echo "Updateing git repo: $base"
 	git pull
 else
+	cd $MY_ENV_CONF
 	echo "Init (Cloning) git repo: $base"
 	git clone git://github.com/seebi/dircolors-solarized.git
 fi
@@ -18,6 +19,6 @@ cp $base/dircolors.ansi-universal $target
 echo "color setting copied, source: $base/dircolors.ansi-universal, target: $target"
 
 if [ -z `readlink -e $target` ] ; then
-	cp $MY_CONF/dircolors_basic/dir_colors $target
+	cp $MY_ENV_CONF/dircolors_basic/dir_colors $target
 	echo "Warn: seems setup dir color failed, will copy basic one, source: $base/dircolors.ansi-universal, target: $target"
 fi
