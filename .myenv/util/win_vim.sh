8a3de1d863acdbdf8557b8e9d0847e98b73be7ff
stico <ouyzhu@gmail.com>
2013-01-07 11:29:20 +0800

    -m

--- .myenv/util/win_vim.sh
+++ .myenv/util/win_vim.sh
@@ -4,10 +4,10 @@ vimBin=${MY_PRO}/A_Text_Vim_7.2_PA-Basic/App/vim/vim72/gVim
 
 [ ! -n "$1" ] && echo '-!> At least set the 1st parameter, which indicating the file to edit' && exit
 
-if [[ $(echo $1 | grep -c "^/cygdrive/") == 1 ]] ; then
-	cygpath -w $1
+if [ $(echo $1 | grep -c "^/") -eq 1 ] ; then
 	winPath=`cygpath -w $1`
 	shift
 fi
 
+echo "INFO: invoking ${vimBin} $winPath $* &"
 ${vimBin} $winPath $* &
