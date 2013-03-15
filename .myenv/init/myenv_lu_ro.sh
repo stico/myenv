f1d130c18f0ec82826ef130331aeca438c02f18e
ouyzhu@workvm <ouyzhu@workvm>
2013-03-15 14:10:42 +0800

    updates

--- .myenv/init/myenv_lu_ro.sh
+++ .myenv/init/myenv_lu_ro.sh
@@ -9,15 +9,38 @@ git_myenv_addr=https://github.com/stico/myenv.git
 [ -e $apt_update_stamp ] && (( $apt_update_ago > 86400 )) && sudo apt-get update || echo "INFO: last 'apt-get update' was $apt_update_ago seconds ago, skip this time"
 
 sudo apt-get install -y git subversion
-sudo apt-get install -y tree zip
+sudo apt-get install -y tree zip unzip
 
-cd ~
-if [ -e ~/.git ] ; then
-	git pull
-else
-	git clone $git_myenv_addr
-	mv ~/$git_myenv_name/* ~
-	mv ~/$git_myenv_name/.* ~
-	rm -rf ~/$git_myenv_name/
-fi
+function init_with_git {
+	echo "INFO: git command exist, use git way"
+
+	cd ~
+	if [ -e ~/.git ] ; then
+		echo "INFO: myenv repository alread exist, simple update it"
+		git pull
+	else
+		git clone $git_myenv_addr
+		mv ~/$git_myenv_name/* ~
+		mv ~/$git_myenv_name/.* ~
+		rm -rf ~/$git_myenv_name/
+	fi
+}
+
+function init_without_git {
+	echo "INFO: git command NOT exist, use zip way"
+	
+	tmp_dir=/tmp/myenv_tmp_init
+	dir_name=myenv-master
+	pkg_name=master.zip
+
+	rm -rf $tmp_dir
+	mkdir -p $tmp_dir
+	cd $tmp_dir
+	wget http://github.com/stico/myenv/archive/$pkg_name
+	unzip $pkg_name
+	mv -f $tmp_dir/$dir_name/* ~
+	mv -f $tmp_dir/$dir_name/.* ~
+}
+
+(command -v git &> /dev/null) && init_with_git || init_without_git
 echo "INFO: myenv init success, you need to re-login shell!"
