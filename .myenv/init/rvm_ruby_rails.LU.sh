364598a7ed04d12294c0e361d9b09df882f830e7
ouyzhu@workvm <ouyzhu@workvm>
2013-04-23 10:37:05 +0800

    updates

--- .myenv/init/rvm_ruby_rails.LU.sh
+++ .myenv/init/rvm_ruby_rails.LU.sh
@@ -7,6 +7,7 @@ sudo apt-get install -y libsqlite3-dev nodejs
 \curl -L https://get.rvm.io | bash -s stable
 echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc
 source ~/.rvm/scripts/rvm
+command rvm autolibs enable
 command rvm install ruby-1.9.3-p327
 rvm use ruby-1.9.3-p327@global --default
 gem install rails
