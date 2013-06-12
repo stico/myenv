3131ffd21c08c8f8145aa3eba1506afb699f7ae8
ouyzhu@workvm <ouyzhu@workvm>
2013-06-12 14:30:58 +0800

    add vim compile script

---
+++ .myenv/init/vim.compile.LU.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Var
+vim_source=/tmp/vim_src_hg
+vim_target=~/program/vim_73_compile
+mak_file=${vim_source}/src/Make_mvc.mak
+
+# Prepare
+sudo apt-get build-dep vim
+sudo apt-get install -y libncurses5-dev
+# note: seems libncursesw5-dev not work
+# more: libgtk2.0-dev libx11-dev xorg-dev
+# more? exuberant-ctags rake python-dev git-core wget sed ack-grep 
+[ ! -e ${mak_file}.bak ] && cp ${mak_file} ${mak_file}.bak
+
+mkdir -p $vim_source; cd $vim_source 
+[ -e $vim_source/.hg ] && hg pull && hg update || hg clone https://vim.googlecode.com/hg/ .
+
+# Compile
+make distclean		# for configure 
+make clean		# for build
+# CFLAGS to avoid the -g flag. More options: --enable-perlinterp --enable-python3interp --enable-tclinterp --enable-rubyinterp
+#CFLAGS="-O2" LDFLAGS="-static" ./configure \		# cause "no terminal library found"
+#env CFLAGS="-O2" LDFLAGS="-static" ./configure \	# cause "no terminal library found"
+# TODO: how to make link the vim statically?
+./configure \
+--prefix=$vim_target \
+--with-features=huge \
+--enable-pythoninterp \
+--enable-rubyinterp \
+--enable-multibyte \
+--enable-gui=gtk2 \
+--enable-fontset \
+--enable-xim
+
+sed -i 's/^RUBY_VER = .*/RUBY_VER = 19/'		$mak_file
+sed -i 's/^RUBY_VER_LONG = .*/RUBY_VER_LONG = 1.9/'	$mak_file
+make install
+
+# record the compile options
+cp $MY_ENV/init/`basename $0` $vim_target 
