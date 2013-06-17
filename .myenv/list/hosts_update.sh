52a6f274f6b357f325f08a5851236947fce19c21
ouyzhu@workvm <ouyzhu@workvm>
2013-06-17 18:20:21 +0800

    hosts_update script small update

--- .myenv/list/hosts_update.sh
+++ .myenv/list/hosts_update.sh
@@ -5,13 +5,10 @@ target=$LOC_HOSTS
 line_begin='########## myenv host append begin (DO NOT update this line) ##########'
 line_end='########## myenv host append end (DO NOT update this line) ##########'
 
-if [[ -e $source && -e $target ]] ; then
-	echo -e "Updating host list file.\nSource: $source \nTarget: $target"
-else
-	echo -e "\nError: $source or $target not exists, pls check! Exit..." 
-	exit 1
-fi
+[ ! -e "$source" -o ! -e "$target" ] && echo "Error: $source or $target not exists, try 'sudo -E xxx'" && exit 1
+[ ! -w $target ] && echo "ERROR: must use sudo to update $target" && exit 1
 
+echo -e "Updating host list file.Source: $source Target: $target"
 sed -i -e "/$line_begin/,/$line_end/d" $target
 echo -e "\n\n\n\n${line_begin}" >> $target
 sed -e "/^\s*$/d" $source >> $target
