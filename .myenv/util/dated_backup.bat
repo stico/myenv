f9fdb73acf1692babded6cd5fec7fac39f381aa8
stico <ouyzhu@gmail.com>
2013-06-26 17:40:55 +0800

    update dated del bak into function

--- .myenv/util/dated_backup.bat
+++ .myenv/util/dated_backup.bat
@@ -1,4 +1,8 @@
 @ECHO OFF
 REM the .sh must use full path, and must use ' around %1, incase there are blanks
-bash %MY_ENV_UTIL%\dated_backup.sh %1
+REM bash %MY_ENV_UTIL%\dated_backup.sh %1
+
+REM After lots try, this the way to support chinese and blank in path
+%MY_PRO%\A_System_Cygwin\bin\cygpath -u %1 | bash -c 'read -e target; source ${HOME}/.myenv/env_func_bash; func_backup_dated "${target}"'
+
 PAUSE
--- .myenv/util/dated_delete.bat
+++ .myenv/util/dated_delete.bat
@@ -1,3 +1,7 @@
 @ECHO OFF
 REM the .sh must use full path, and must use ' around %1, incase there are blanks
-bash %MY_ENV_UTIL%\dated_delete.sh %1
+REM bash %MY_ENV_UTIL%\dated_delete.sh %1
+
+REM After lots try, this the way to support chinese and blank in path
+%MY_PRO%\A_System_Cygwin\bin\cygpath -u %1 | bash -c 'read -e target; source ${HOME}/.myenv/env_func_bash; func_delete_dated "${target}"'
+
