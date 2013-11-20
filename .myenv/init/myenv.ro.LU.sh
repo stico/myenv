c8ff15fe7909d5fb31015e77d9a4829551ce14d1
stico <ouyzhu@gmail.com>
2013-11-20 12:31:35 +0800

    improve myenv init script

--- .myenv/init/myenv.ro.LU.sh
+++ .myenv/init/myenv.ro.LU.sh
@@ -13,7 +13,8 @@ function init_git {
 	[ -e ${HOME}/dev/git ] && echo "INFO: git already exist, skip init git" && return 0
 
 	# Try install by system
-	(sudo -n ls &> /dev/null) && sudo apt-get update && sudo apt-get install -y libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev build-essential tree zip unzip subversion git > /dev/null && return 0
+	(sudo -n ls &> /dev/null) && sudo apt-get update 
+	(sudo -n ls &> /dev/null) && sudo apt-get install -y libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev build-essential tree zip unzip subversion git > /dev/null && return 0
 
 	# Try compile
 	git_tar="git-1.8.4.3.tar.gz"
