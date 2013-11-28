995251ee59f7c3cb6952a4f6b646956b0097ea12
stico <ouyzhu@gmail.com>
2013-11-28 13:48:25 +0800

    vim compile script improve, function improve

--- .bashrc
+++ .bashrc
@@ -12,41 +12,12 @@
 [ "$os_cygwin" = "false" -a -f /etc/bash_completion ] && source /etc/bash_completion 	# very slow in cygwin, run it first, init/bash.sh need turn off some completion on cygwin
 SHELL="/bin/bash" [ -f ~/.dir_colors ] && eval `dircolors -b ~/.dir_colors` || eval `dircolors -b /etc/DIR_COLORS`
 
-# platform depended operation
-if [ "$os_cygwin" = "false" ] ; then
-
-	# Auto complete (2013-09-9, LM15 need use ssh <tab> to "init" _ssh, otherwise not work, why?)
-	complete -o default -o nospace -F _scp scpx
-	complete -o default -o nospace -F _ssh sshx
-	complete -o default -o nospace -F _ssh ssht
-	#`complete | grep -q " dd$"` && complete -r dd			# Check before remove, since alias conflict with /bin/dd, disable /bin/dd complete. 
-	#complete -r vi vim gvim unzip					# vi complete seems very annoying (shows help of gawk!) on cygwin # seems fix in cygwin 1.17
-
-	# set diff prompt for internal machine and external machine 
-	internetIpCount=$(/sbin/ifconfig | sed -n -e '/inet addr:/p' | grep -v -c '\(172\.\|192\.\|10\.\|127.0.0.1\)')
-	if `grep -q "bash_prompt_color=green" ~/.myenv/zgen/sys_info_local &> /dev/null` ; then
-		# Green line with $ in same line
-		export PS1="\[\e[32m\]\u@\h \[\e[32m\]\w\$\[\e[0m\]"
-	elif [ $internetIpCount -ge 1 ] ; then 
-		# Red line with $ in next line
-		export PS1="\[\e[31m\]\u@\h \[\e[31m\]\w\[\e[0m\]\n\$"
-	else 
-		# Blue line with $ in same line
-		export PS1="\[\e[34m\]\u@\h \[\e[34m\]\w\$\[\e[0m\]"
-	fi
-else
-	# Green line with $ in same line
-	export PS1="\[\e[32m\]\u@\h \[\e[32m\]\w\$\[\e[0m\]"
-fi
-
 # If not running interactively, don't do anything
 [ -z "$PS1" ] && return
 
-# Init myenv
+# Init myenv, including common functions
 source $HOME/.myenv/init/bash.sh
 [ -e $HOME/.bashrc_local ] && source $HOME/.bashrc_local
-
-# call functions
 func_ssh_agent_init
 
 stty -ixon		# avoid ^s/^q to frozen/unfrozen terminal (so vim could also use those keys)
@@ -57,17 +28,32 @@ shopt -s histappend	# append to the history file, don't overwrite it
 shopt -s histreedit	# puts a failed history substitution back on the command line for re-editing
 #shopt -s histverify	# (caution to use it, since most system not use it, train yourself that way) puts the command to be executed after a substitution on command line as if you had typed it
 
-# INFINALITY FONT RENDERING CONFIG
-[ -e /etc/infinality-settings.sh ] && . /etc/infinality-settings.sh
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"					# make less more friendly for non-text input files, see lesspipe(1)
+[ -e /etc/infinality-settings.sh ] && . /etc/infinality-settings.sh				# infinality font rendering config
+[ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ] && debian_chroot=$(cat /etc/debian_chroot)	# set variable identifying the chroot you work in (used in the prompt below)
 
-########################## Below are just copied from example file
+# platform depended operation
+if [ "$os_cygwin" = "false" ] ; then
+	# Auto complete (2013-09-9, LM15 need use ssh <tab> to "init" _ssh, otherwise not work, why?)
+	complete -o default -o nospace -F _scp scpx
+	complete -o default -o nospace -F _ssh sshx
+	complete -o default -o nospace -F _ssh ssht
+	#`complete | grep -q " dd$"` && complete -r dd			# Check before remove, since alias conflict with /bin/dd, disable /bin/dd complete. 
+	#complete -r vi vim gvim unzip					# vi complete seems very annoying (shows help of gawk!) on cygwin # seems fix in cygwin 1.17
 
-# make less more friendly for non-text input files, see lesspipe(1)
-[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
-
-# set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
+	# set diff prompt for internal machine and external machine 
+	internetIpCount=$(func_ip | grep -v -c '^\(172\.\|192\.\|10\.\|127.0.0.1\)')
+	if `grep -q "bash_prompt_color=green" ~/.myenv/zgen/sys_info_local &> /dev/null` ; 
+	then
+		export PS1="\[\e[32m\]\u@\h \[\e[32m\]\w\$\[\e[0m\]"	# Green line with $ in same line
+	elif [ "$internetIpCount" -ge 1 ] ; then 
+		export PS1="\[\e[31m\]\u@\h \[\e[31m\]\w\[\e[0m\]\n\$"	# Red line with $ in next line
+	else 
+		export PS1="\[\e[34m\]\u@\h \[\e[34m\]\w\$\[\e[0m\]"	# Blue line with $ in same line
+	fi
+else
+	# Green line with $ in same line
+	export PS1="\[\e[32m\]\u@\h \[\e[32m\]\w\$\[\e[0m\]"
 fi
 
 ################################################################################
--- .myenv/conf/vim/init.compile.LU.sh
+++ .myenv/conf/vim/init.compile.LU.sh
@@ -1,42 +1,50 @@
 #!/bin/bash
 
+# TODO: add local install 
+# TODO: how to make link the vim statically?
+# TODO: 2013-06-12: why the compiled vim start mode is REPLACE?
 # INFO: target path must be fixed, otherwise vim can not load files in runtime
 
-# Pre-check
-(! command -v ruby &> /dev/null) && echo "ERROR: Seems no ruby installed, pls check!" && exit 1
-(! command -v python &> /dev/null) && echo "ERROR: Seems no python installed, pls check!" && exit 1
-[ ! -e $MY_ENV/env_func_bash ] && echo "ERROR: $MY_ENV/env_func_bash not exist, pls check!" && exit 1
+# Load common function
+common_func_env=~/.myenv/env_func_bash 
+common_func_tmp=/tmp/env_func_bash 
+common_func_url="https://raw.github.com/stico/myenv/master/.myenv/env_func_bash" 
+cp -f "$common_func_env" "$common_func_tmp" || wget -O "$common_func_tmp" -q "$common_func_url" 
+[ ! -e "$common_func_tmp" ] && echo "ERROR: $common_func_tmp not exist" && exit 1|| source "$common_func_tmp" 
 
-# Prepare - Var
-vim_source=/tmp/vim_src_hg
-mak_file=${vim_source}/src/Make_mvc.mak
-source $MY_ENV/env_func_bash 
-sys_info=`func_sys_info_os_name``func_sys_info_os_ver`_`func_sys_info_os_len`
+# Pre-check
+func_validate_cmd_exist ruby
+func_validate_cmd_exist python
+func_param_check 1 "USAGE: $0 <remote|local>" "$@" 
+
+# Prepare - source. 
+if [ "$1" = "remote" ] ; then
+	source_base=/tmp/vim_src_hg
+	source_makefile=${source_base}/src/Make_mvc.mak
+	func_mkdir_cd "${source_base}"
+	[ -e $source_base/.hg ] && hg pull && hg update || hg clone https://vim.googlecode.com/hg/ .
+fi
 
 # Prepare - dependencies
+# more: libgtk2.0-dev libx11-dev xorg-dev
+# more? exuberant-ctags rake git-core wget sed ack-grep 
 sudo apt-get build-dep vim
-sudo apt-get install -y libncurses5-dev libtinfo-dev 
 sudo apt-get install -y libgtk2.0-dev libx11-dev xorg-dev	# for linuxmint (otherwise reports not GUI support and no gvim created)
-								# more: libgtk2.0-dev libx11-dev xorg-dev
-								# more? exuberant-ctags rake python-dev git-core wget sed ack-grep 
+sudo apt-get install -y python-dev libncurses5-dev libtinfo-dev 
 
-# Hg src
-mkdir -p $vim_source; cd $vim_source 
-[ -e $vim_source/.hg ] && hg pull && hg update || hg clone https://vim.googlecode.com/hg/ .
+# Variables
+source_ver=`sed -n -e '/^[vV]im\|^\s\|^$/!q;s/\s*Version \(\S*\)\..*/\1/p' $source_base/Contents`
+target_base=~/program/vim-${source_ver}_`date "+%Y%m%d"`_$(func_sys_info)
 
-# Get version
-vim_ver=`sed -n -e '/^[vV]im\|^\s\|^$/!q;s/\s*Version \(\S*\)\..*/\1/p' $vim_source/Contents`
-vim_target=~/program/vim-${vim_ver}_`date "+%Y%m%d"`_${sys_info}
+# Clean up
+cd "$source_base" && make distclean && make clean
 
 # Configure
-make distclean		# for configure 
-make clean		# for build
-
-								#CFLAGS="-O2" LDFLAGS="-static" ./configure \			# CFLAGS to avoid the -g flag. Failed: cause "no terminal library found", install libncurses5-dev/libncursesw5-dev not works since already installed
-								#env CFLAGS="-O2" LDFLAGS="-static" ./configure \		# CFLAGS to avoid the -g flag. Failed: cause "no terminal library found", install libncurses5-dev/libncursesw5-dev not works since already installed
-# TODO: how to make link the vim statically?
+#CFLAGS="-O2" LDFLAGS="-static" ./configure \			# CFLAGS to avoid the -g flag. Failed: cause "no terminal library found", install libncurses5-dev/libncursesw5-dev not works since already installed
+#env CFLAGS="-O2" LDFLAGS="-static" ./configure \		# CFLAGS to avoid the -g flag. Failed: cause "no terminal library found", install libncurses5-dev/libncursesw5-dev not works since already installed
+# more: --enable-perlinterp --enable-python3interp --enable-tclinterp --enable-rubyinterp
 ./configure \
---prefix=$vim_target \
+--prefix=$target_base \
 --with-features=huge \
 --enable-pythoninterp \
 --enable-rubyinterp \
@@ -44,17 +52,14 @@ make clean		# for build
 --enable-gui=gtk2 \
 --enable-fontset \
 --enable-xim
-								#more: --enable-perlinterp --enable-python3interp --enable-tclinterp --enable-rubyinterp
 
-[ ! -e ${mak_file}.bak ] && cp ${mak_file} ${mak_file}.bak
-sed -i 's/^RUBY_VER = .*/RUBY_VER = 20/' $mak_file
-sed -i 's/^RUBY_VER_LONG = .*/RUBY_VER_LONG = 2.0/' $mak_file
+[ ! -e ${source_makefile}.bak ] && cp ${source_makefile}{,.bak}
+sed -i "s/^RUBY_VER = .*/RUBY_VER = 20/" $source_makefile
+sed -i "s/^RUBY_VER_LONG = .*/RUBY_VER_LONG = 2.0/" $source_makefile
 
 # Make
 make -j 2	# use 2 cpu core to compile
 make install
 
 # Record the compile options
-cp $MY_ENV/init/`basename $0` $vim_target 
-
-# TODO: 2013-06-12: why the compiled vim start mode is REPLACE?
+cp ~/.myenv/conf/vim/$(basename $0) $target_base 
