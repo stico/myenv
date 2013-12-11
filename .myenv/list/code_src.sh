f568b02e5434b921ad908214cb4fad55f917b2e3
stico <ouyzhu@gmail.com>
2013-12-11 11:26:00 +0800

    improve func_ssh_agent_init, update shortcuts

---
+++ .myenv/list/code_src.sh
@@ -0,0 +1,6 @@
+declare -A repos
+repos["springside"]="https://github.com/springside/springside4"
+repos["spring-framework"]="https://github.com/spring-projects/spring-framework"
+repos["apache_archetypes_trunk"]="http://svn.apache.org/repos/asf/maven/archetypes/trunk"
+
+# TODO: add script to auto init/update
