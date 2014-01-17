d3149207b1ddc56c439f5ca5decc1864d3b45571
stico <ouyzhu@gmail.com>
2014-01-17 12:16:32 +0800

    exclude A_NOTE_Copy.txt in collect_note, since might be polluted

--- .myenv/myenv_func.sh
+++ .myenv/myenv_func.sh
@@ -623,7 +623,7 @@ function func_collect_note {
 
 	# Note, used "ERE (Extended Regex) to avoid passing "\" around)
 	include_patterns=(A_NOTE --NOTE-- --NOTED-- .bash$ .sh$)
-	exclude_patterns=(.rtf$ .lnk$ /DCB/DatedBackup/ thunderbird.*trash)
+	exclude_patterns=(.rtf$ .lnk$ /DCB/DatedBackup/ thunderbird.*trash A_NOTE_Copy.txt)
 	func_collect_files $target_base $source_bases $source_quick_link $include_patterns $exclude_patterns
 }
 
