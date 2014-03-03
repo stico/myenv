182432e0c040e3c81c35c140ef7256f177071e69
stico <ouyzhu@gmail.com>
2014-03-03 12:01:40 +0800

    add tool concept

--- .myenv/tool/wine/init/install_foxit.sh
+++ .myenv/tool/wine/init/install_foxit.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# Just directly run to install
+# NOTE
+#	Just directly run to install
+#
+#	Truble Shooting
+#		FoxitReader614.0217_enu_Setup.exe
+#			Unable to register the DLL/OCX: RegSvr32 failed with exit code
+#			(and some others alike)
+#			-> select "Ignore"
 wine $MY_DOC/DCB/Software/installer/FoxitReader614.0217_enu_Setup.exe
