17f91168b10d663e8deedeeb73214815f6e7ecc4
ouyzhu <ouyzhu@gmail.com>
2014-08-19 15:09:48 +0800

    remove unison config

--- .myenv/list/code_src.sh
+++
@@ -1,6 +0,0 @@
-declare -A repos
-repos["springside"]="https://github.com/springside/springside4"
-repos["spring-framework"]="https://github.com/spring-projects/spring-framework"
-repos["apache_archetypes_trunk"]="http://svn.apache.org/repos/asf/maven/archetypes/trunk"
-
-# TODO: add script to auto init/update
