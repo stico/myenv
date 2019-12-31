e98a46ddc486d039cc748a526cdccbbdfd6b1856
ouyzhu <ouyzhu@gmail.com>
2019-12-31 16:03:04 +0800

    small updates

--- .myenv/myenv_lib.sh
+++ .myenv/myenv_lib.sh
@@ -104,7 +104,7 @@ func_uncompress() {
 	func_complain_path_exist "${target_dir}" && return	# seem NOT need exit, just complain is enough?
 
 	echo "INFO: uncompress file, from: ${source_file} to: ${target_dir}"
-	func_mkdir_cd "${2}"
+	func_mkdir_cd "${target_dir}"
 	case "$source_file" in
 		*.jar | *.arr | *.zip)
 				func_complain_cmd_not_exist unzip \
