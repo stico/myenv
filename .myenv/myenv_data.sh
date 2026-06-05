cde39440c9b8390837bf2a4153751c4001866bd9
ouyangzhu <ouyangzhu@yy.com>
2026-06-05 11:11:56 +0800

    update from deskmac6

--- .myenv/myenv_data.sh
+++ .myenv/myenv_data.sh
@@ -6,7 +6,7 @@ source $HOME/.myenv/myenv_lib.sh || eval "$(wget -q -O - "https://raw.github.com
 #	ts	tab separate, default separator for data fields
 #	kv	key-value
 
-data_validate_kv_file_format() {
+data_complain_kv_file_format() {
 	# number of columns (1st line as standard)
 	local cols=$(head -n1 "${1}" | awk -F $'\t' '{print NF}')
 
@@ -15,32 +15,37 @@ data_validate_kv_file_format() {
 		/^$/ { exit 1 }
 	' "${1}"
 
-	[ "$?" -ne "0" ] && echo "ERROR: format error: no TAB found, or col count inconsistant: $(basename "${1}")" >&2 && exit 1
+	[ "$?" -ne "0" ] && echo "ERROR: format error: no TAB found, or col count inconsistant: $(basename "${1}")" >&2 && return 1
 }
 
-data_validate_kv_file_confliction() {
-	awk -F'\t' '
+data_complain_kv_file_confliction() {
+	awk -F $'\t' '
 		!seen[$0]++ {					# 过滤掉完全重复的行 (注意这里是"整行比较")
 			if ($1 in kv && kv[$1] != $2) {		# 如果这个 Key 之前已经存过不同的 Value，说明不一致
 				print "ERROR: Key [" $1 "] has diff value: [" kv[$1] "] V.S. [" $2 "]"
 				has_error = 1
 			}
-			kv[$1] = $2				# 记录当前 Key 对应的 Value
+			# 记录当前 Key 对应的 Value
+			kv[$1] = $2		
+			counter++
 		}
 		END { if (has_error) exit 1 }
 	' "${1}"
 
-	[ "$?" -ne "0" ] && echo "ERROR: K:V conflict: $(basename "${1}")" >&2 && exit 1
+	[ "$?" -ne "0" ] && echo "ERROR: K:V conflict: $(basename "${1}")" >&2 && return 1
 }
 
-data_validate_kv_file() {
+data_check_kv_file() {
 	local usage="Usage: $FUNCNAME <tsv_file> <keys_to_lookup>"
 	local desc="Desc: file format: <key>\t<value>, for tsv file, 2nd field and after are ALL 'value'" 
 	func_param_check 1 "${desc} \n${usage} \n" "$@"
 
-	func_validate_path_exist "${1}"
-	data_validate_kv_file_format "${1}"
-	data_validate_kv_file_confliction "${1}"
+	local fail="false"
+	func_complain_path_not_exist "${1}"		&& fail="true" 
+	data_complain_kv_file_format "${1}"		&& fail="true"
+	data_complain_kv_file_confliction "${1}"	&& fail="true" 
+
+	[[ "${fail}" == "true" ]] && echo "ERROR: kv file check failed!" && return 1
 }
 
 data_kv_lookup() {
@@ -52,7 +57,7 @@ data_kv_lookup() {
 	kv_table="$(mktemp)"
 
 	# check and gen kv_table
-	data_validate_kv_file "${kv_table}"
+	data_check_kv_file "${kv_table}" || return 1
 	func_shrink_dup_lines "${1}" > "${kv_table}"
 	echo "INFO: using generated kv_table file: ${kv_table}" >&2
 	shift
